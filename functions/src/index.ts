803cfe01c39256626555a28a0744a1d1c8dba168
Brock Taban <hello@brocktaban.com>
2019-04-19 16:38:15 -0400

    Added Cooldown Feature
    Fixed typo

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,24 @@
 import * as functions from 'firebase-functions'
-// import * as admin from 'firebase-admin'
+import * as admin from 'firebase-admin'
 
-// export const liveLastCommand
-//     = functions
-//         .firestore
-//         .document('live/{channelName}/{channelCollections}/{commandId}')
-//         .onWrite((change, context) => {
-//             const data = change.before.data()
-//             const command = context.params.channelCollections == "commands"
-//             const last = context.params.commandId == "last"
+const db = admin.firestore()
 
-//             if (data == undefined || !command || !last)
-//                 return null
+export const liveLastCommand
+    = functions
+        .firestore
+        .document('live/{channelName}/{channelCollections}/{commandId}')
+        .onWrite((change, context) => {
+            const data = change.before.data()
+            const channelName = context.params.channelName
+            const command = context.params.channelCollections == "commands"
+            const last = context.params.commandId == "last"
 
-//             return change.before.ref.parent.add(data)
-//         })
+            if (data == undefined || !command || !last)
+                return null
+
+            const timestamp = data.timestamp
+
+            return db
+                .doc(`live/${channelName}`)
+                .set({lastCommand: timestamp}, {merge: true})
+        })
--- src/twitch.ts
+++ src/twitch.ts
@@ -1,4 +1,5 @@
 import * as tmi from 'tmi.js'
+import { firestore } from 'firebase-admin'
 import { Firebase } from './firebase'
 
 const env = require('../env.json')
@@ -10,8 +11,7 @@ export class Twitch {
     constructor() { this.init() }
 
     private async init() {
-        const channels = await database.getChannles()
-        const commands = await database.getCommands()
+        const channels = await database.getChannes()
 
         const opts = {
             options: {
@@ -20,26 +20,43 @@ export class Twitch {
             identity: env,
             channels: channels
         };
-        
+
         const client = tmi.client(opts)
-        
-        client.on('message', (target, context, msg, self) => {
+
+        client.on('message', async (target, context, msg, self) => {
             if (self) return
-        
-            const channelName = target.replace('#', '') 
+            if (msg.charAt(0) != '?') return
+
+            const channelName = target.replace('#', '')
             const username = context.username
-    
+
+            const channel = await database.getChannel(channelName)
+            const channelData = channel.data()
+
+            if (!channel.exists || channelData == undefined)
+                return client.say(target, `@${username}, ${target} is not playing Valerian Games`)
+                
+            const commands = await database.getGameCommands(channelData.playing)
+
+            const cooldown: number = channelData.loopInMills // 600000
+            const timestamp: firestore.Timestamp = channelData.lastCommand
+            const currentTime = new Date()
+
+            const diffence = currentTime.getTime() - timestamp.toMillis()
+
+            console.log(diffence, cooldown, cooldown > diffence)
+
+            if (cooldown > diffence) 
+                return
+
             commands.forEach(async c => {
                 if (msg.trim() == c.trim()) {
-                    
-                    // if (canCommand) {
-                    //     client.say(target, `${c} it is!`);
-                    //     database.live(channelName, c, username)
-                    // }
-                } 
+                    client.say(target, `${c} it is!`);
+                    database.live(channelName, c, username)
+                }
             });
         });
-    
+
         client.connect()
     }
 }
