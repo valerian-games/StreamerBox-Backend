18b326708df194bbf87f1516f44cb9ace33c1d5c
Brock Taban <hello@brocktaban.com>
2019-04-18 22:51:34 -0400

    Created Firebase Functions
    Secured oAuth

---
+++ functions/src/index.ts
@@ -0,0 +1,15 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+export const liveLastCommand
+    = functions
+        .firestore
+        .document('live/{channelName}/command/last')
+        .onWrite((change, context) => {
+            const data = change.before.data()
+
+            if (data == undefined)
+                return null
+
+            return change.before.ref.parent.add(data)
+        })
