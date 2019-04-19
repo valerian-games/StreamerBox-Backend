25c6ce6dfa11e7ae99ee69950e3c705af5ad802d
Brock Taban <hello@brocktaban.com>
2019-04-19 15:37:48 -0400

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,17 @@
 import * as functions from 'firebase-functions'
 // import * as admin from 'firebase-admin'
 
-export const liveLastCommand
-    = functions
-        .firestore
-        .document('live/{channelName}/{channelCollections}/{commandId}')
-        .onWrite((change, context) => {
-            const data = change.before.data()
-            const command = context.params.channelCollections == "commands"
-            const last = context.params.commandId == "last"
+// export const liveLastCommand
+//     = functions
+//         .firestore
+//         .document('live/{channelName}/{channelCollections}/{commandId}')
+//         .onWrite((change, context) => {
+//             const data = change.before.data()
+//             const command = context.params.channelCollections == "commands"
+//             const last = context.params.commandId == "last"
 
-            if (data == undefined || !command || !last)
-                return null
+//             if (data == undefined || !command || !last)
+//                 return null
 
-            return change.before.ref.parent.add(data)
-        })
+//             return change.before.ref.parent.add(data)
+//         })
